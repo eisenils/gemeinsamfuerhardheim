5bb7c023c9898ffcae59c5f79359793ae8af72a8
eisenils <mail.eisenils@gmail.com>
2020-03-30 14:42:49 +0200

    Implement Socials and display active route.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,6 +2,20 @@ import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { Link } from "@reach/router";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+
+const MenuItem = props => (
+  <Link
+    {...props}
+    getProps={({ isCurrent }) => {
+      return {
+        style: {
+          color: isCurrent ? "#e73a33" : "#333"
+        }
+      };
+    }}
+  />
+);
 
 class Navigation extends Component {
   render() {
@@ -14,11 +28,46 @@ class Navigation extends Component {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="/">Startseite</Nav.Link>
-              <Nav.Link href="/corona">Corona Virus</Nav.Link>
-              <Nav.Link href="/ueber-uns">Über uns</Nav.Link>
+              <Nav.Link>
+                <MenuItem to="/" className="nounderline">
+                  Startseite
+                </MenuItem>
+              </Nav.Link>
+              <Nav.Link>
+                <MenuItem to="/corona" className="nounderline">
+                  Corona Virus
+                </MenuItem>
+              </Nav.Link>
+              <Nav.Link>
+                <MenuItem to="/ueber-uns" className="nounderline">
+                  Über uns
+                </MenuItem>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
+          <Nav>
+            <Nav.Link disabled style={{ color: "#aaa" }}>
+              <i>#gemeinsamfürhardheim</i>
+            </Nav.Link>
+            <Nav.Link>
+              <a
+                href="https://www.facebook.com/Hardheim112-425450161608511/"
+                className="nounderline"
+                style={{ color: "#333" }}
+              >
+                <FaFacebook />
+              </a>
+            </Nav.Link>
+            <Nav.Link>
+              <a
+                href="https://www.instagram.com/hardheim112/"
+                className="nounderline"
+                style={{ color: "#333" }}
+              >
+                <FaInstagram />
+              </a>
+            </Nav.Link>
+          </Nav>
         </Navbar>
       </>
     );
